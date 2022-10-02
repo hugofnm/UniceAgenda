3ef25b800acff567a303cb4fe3d102d27d2df016
Hugo Meleiro <contact@hugofnm.fr>
2022-10-02 15:53:20 +0200

    Update firebase

---
+++ script.js
@@ -0,0 +1,65 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCUIi0m91uGMH6PHpcUIDyTaytI2lSsCHI",
+  authDomain: "todoapp-metrix.firebaseapp.com",
+  databaseURL: "https://todoapp-metrix-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "todoapp-metrix",
+  storageBucket: "todoapp-metrix.appspot.com",
+  messagingSenderId: "599833567578",
+  appId: "1:599833567578:web:1f128fba15adfae2e3d363",
+  measurementId: "G-C7W5CCCRY7"
+};
+
+const notes = document.querySelector(".notes");
+
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+var db = firebase.database().ref("agendaGEII")
+
+document.getElementById("ajoutDevoir").addEventListener("submit", submitForm);
+
+//add function 
+function submitForm(e) {
+  e.preventDefault();
+  var matiere = getElementVal("matiere");
+  var devoir = getElementVal("devoir");
+  var date = getElementVal("date");
+  var ds = getElementVal("ds");
+
+  saveHomework(matiere, devoir, date, ds);
+  alert("Devoir ajouté !");  
+
+  document.getElementById("ajoutDevoir").reset();
+  location.reload();
+}
+
+const saveHomework = (matiere, devoir, date, ds) => {
+  var newHomework = db.push();
+
+  newHomework.set({
+    matiere: matiere,
+    devoir: devoir,
+    date: date,
+    ds: ds
+  });
+}
+
+const getElementVal = (id) => {
+  return document.getElementById(id).value;
+}
+
+db.on("value", function(snapshot) {
+  snapshot.forEach(function(childSnapshot) {
+   var childData = childSnapshot.val();
+   var id=childData.id;
+   console.log(childData);
+
+   const div = document.createElement("div");
+   const text = document.createTextNode(`${childData.ds} | ${childData.matiere} | ${childData.devoir} | Pour le : ${childData.date}`);
+   div.classList.add("container2");
+   div.classList.add("noteText");
+
+   div.classList.add("blur");
+   div.appendChild(text);
+   notes.append(div);
+  });
+ });
